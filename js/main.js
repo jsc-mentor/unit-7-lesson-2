9f1289a65803679ba22dbccc0772449c8d386d61
sarah-ga <sarah.hallacher@generalassemb.ly>
2016-08-12 16:05:24 -0400

    unit-7-lesson-2

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Step 1: When the window is resized, add the snazzy class to the body.
+// Then resize the browser window to see this in action!
+		// Hint: Note that when selecting the window, no quotes are needed around the word window 
+		// in the jQuery selector, like we need in other selectors
+		// Here's an example:
+				// $(window).on('yourEventTypeWillGoHere', function () {
+
+				// });
+		// You'll want to replace yourEventTypeWillGoHere with the type of event you're listening for.
+
+
+// STEP 2. When the user's mouse enters #trigger (the yellow square)
+	// a) Add a <li> to the beginning of #myList that contains the text "1" (Hint: you'll need to use the prepend jquery method)
+	// b) Add a <li> to the end of #myList that contains the text "5" (Hint: you'll need to use the append jquery method)
+
+
+// STEP 3: When the user scrolls down the window (hint our jquery selector will be $(window) like in example #1)
+	// a) Change the text in the h1 to "Scroll time!"
+	// b) Update the src for the img to 'images/outdoors.jpeg'
